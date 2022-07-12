bfdbb847f5c217576268bf32cd9ad85c2cb3e315
ThawingX <ThawingX@gmail.com>
2022-07-12 16:51:26 +0800

    fix:change the footer url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
     <a
       icon-btn i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/thawingx"
       target="_blank"
       title="GitHub"
     />
